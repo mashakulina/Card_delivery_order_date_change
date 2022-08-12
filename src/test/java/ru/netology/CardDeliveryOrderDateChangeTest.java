c0df5a3351b2774447fb1c0347691a51a8053c3a
mashakulina <masha_kulina@mail.ru>
2022-08-12 18:53:25 +0300

    Внесены изменения в тест

--- src/test/java/ru/netology/CardDeliveryOrderDateChangeTest.java
+++ src/test/java/ru/netology/CardDeliveryOrderDateChangeTest.java
@@ -16,26 +16,31 @@ public class CardDeliveryOrderDateChangeTest {
     private final String phone = getPhoneNumber();
     private final String city = getCity();
 
+    @AfterEach
+    void close() {
+        closeWebDriver();
+    }
+
     @Test
     void registration() {
         open("http://localhost:9999");
         $("[placeholder='Город']").setValue(city);
+        $x("//input[@placeholder='Дата встречи']").click();
         $x("//input[@name='name']").setValue(name);
         $("[name='phone']").setValue(phone);
-        $x("//input[@placeholder='Дата встречи']").click();
         $("[data-test-id='agreement']").click();
         $("[class='button__text']").click();
-        $("[type='checkbox']").should(visible, Duration.ofSeconds(15));
+        $("[data-test-id='success-notification']").should(visible, Duration.ofSeconds(15));
     }
 
     @Test
     void newDateForRegistration() {
         open("http://localhost:9999");
         $("[placeholder='Город']").setValue(city);
+        $x("//input[@placeholder='Дата встречи']").click();
         $x("//input[@name='name']").setValue(name);
         $("[name='phone']").setValue(phone);
-        $x("//input[@placeholder='Дата встречи']").click();
-        $("[type='checkbox']").click();
+        $("[data-test-id='agreement']").click();
         $("[class='button__text']").click();
         $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
         $x("//input[@placeholder='Дата встречи']").setValue(getNewDate());
