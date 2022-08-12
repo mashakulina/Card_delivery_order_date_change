4ef1bd21a2d9a591d4463cb63007f16ac3945f39
mashakulina <masha_kulina@mail.ru>
2022-08-12 18:13:16 +0300

    Внесены изменения в тесты

--- src/test/java/ru/netology/CardDeliveryOrderDateChangeTest.java
+++ src/test/java/ru/netology/CardDeliveryOrderDateChangeTest.java
@@ -20,26 +20,21 @@ public class CardDeliveryOrderDateChangeTest {
     void setUp() {
         open("http://localhost:9999");
         Configuration.holdBrowserOpen = true;
-
+        $("[placeholder='Город']").setValue(city);
+        $x("//input[@name='name']").setValue(name);
+        $("[name='phone']").setValue(phone);
     }
 
     @Test
     void registration() {
-        $("[placeholder='Город']").setValue(city);
-        $x("//input[@name='name']").setValue(name);
-        $("[name='phone']").setValue(phone);
         $x("//input[@placeholder='Дата встречи']").click();
         $("[data-test-id='agreement']").click();
         $("[class='button__text']").click();
         $("[data-test-id='success-notification']").should(visible, Duration.ofSeconds(15));
-        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
     }
 
     @Test
     void newDateForRegistration() {
-        $("[placeholder='Город']").setValue(city);
-        $x("//input[@name='name']").setValue(name);
-        $("[name='phone']").setValue(phone);
         $x("//input[@placeholder='Дата встречи']").click();
         $("[data-test-id='agreement']").click();
         $("[class='button__text']").click();
