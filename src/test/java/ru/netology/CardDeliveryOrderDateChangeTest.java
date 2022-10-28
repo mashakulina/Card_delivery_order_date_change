b297805d132888de34e374f35798a53a9e1d6b47
mashakulina <masha_kulina@mail.ru>
2022-10-28 20:21:01 +0300

    Внесены изменения в тест

--- src/test/java/ru/netology/CardDeliveryOrderDateChangeTest.java
+++ src/test/java/ru/netology/CardDeliveryOrderDateChangeTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.Keys;
 import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static ru.netology.DataGenerator.*;
 
@@ -15,6 +16,9 @@ public class CardDeliveryOrderDateChangeTest {
     private final String name = getName();
     private final String phone = getPhoneNumber();
     private final String city = getCity();
+    private final String date = getNewDate(4);
+    private final String otherDate = getNewDate(7);
+    private final String invalidDate = getIncorrectDate();
 
 
     @AfterEach
@@ -35,18 +39,37 @@ public class CardDeliveryOrderDateChangeTest {
     }
 
     @Test
-    void newDateForRegistration() {
+    void shouldGivenNewDateForRegistration() {
         open("http://localhost:9999");
         $("[placeholder='Город']").setValue(city);
-        $x("//input[@placeholder='Дата встречи']").click();
+        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        $x("//input[@placeholder='Дата встречи']").setValue(date);
         $x("//input[@name='name']").setValue(name);
         $("[name='phone']").setValue(phone);
         $("[data-test-id='agreement']").click();
         $("[class='button__text']").click();
+        $("[data-test-id='success-notification']").should(visible, Duration.ofSeconds(15));
         $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
-        $x("//input[@placeholder='Дата встречи']").setValue(getNewDate());
+        $x("//input[@placeholder='Дата встречи']").setValue(otherDate);
         $("[class='button__text']").click();
         $x("//span[contains(text(),'Перепланировать')]").click();
         $("[data-test-id='success-notification']").should(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldNoGivenNewDateForRegistration() {
+        open("http://localhost:9999");
+        $("[placeholder='Город']").setValue(city);
+        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        $x("//input[@placeholder='Дата встречи']").setValue(date);
+        $x("//input[@name='name']").setValue(name);
+        $("[name='phone']").setValue(phone);
+        $("[data-test-id='agreement']").click();
+        $("[class='button__text']").click();
+        $("[data-test-id='success-notification']").should(visible, Duration.ofSeconds(15));
+        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        $x("//input[@placeholder='Дата встречи']").setValue(invalidDate);
+        $("[class='button__text']").click();
+        $(byText("Заказ на выбранную дату невозможен")).should(visible, Duration.ofSeconds(15));
     }
 }
--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -9,6 +9,20 @@ import java.util.Locale;
 public class DataGenerator {
     private static final Faker faker = new Faker(new Locale("ru"));
 
+    public static String getCity() {
+        return faker.address().city();
+    }
+
+    public static String getNewDate(int days) {
+        String date = LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return date;
+    }
+
+public static String getIncorrectDate() {
+        String today = LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return today;
+}
+
     public static String getName() {
         return faker.name().fullName();
     }
@@ -17,14 +31,5 @@ public class DataGenerator {
         return faker.phoneNumber().phoneNumber();
     }
 
-    public static String getNewDate() {
-        LocalDate today = LocalDate.now();
-        LocalDate dateToBeSet = today.plusDays(7);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        return dateToBeSet.format(formatter);
-    }
 
-    public static String getCity() {
-        return faker.address().city();
-    }
 }
